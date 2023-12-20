c2d987a3985692b80b5704175fa7d784e595da34
newproject23 <143152902+newproject23@users.noreply.github.com>
2023-12-20 11:11:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6667859840:AAGRhtQ2KeQyQTuVxSHXWysd9noywr85GU0")
 
    
